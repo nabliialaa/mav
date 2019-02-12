015646c4802dedf431fc6b14d41488b846d012af
nabliialaa <nabliialaa@gmail.com>
2019-02-12 12:17:07 +0100

    First commit

---
+++ src/main/java/com/soprahr/testmavenbuild/App.java
@@ -0,0 +1,13 @@
+package com.soprahr.testmavenbuild;
+import java.io.IOException;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Nabli" );
+    }
+}
